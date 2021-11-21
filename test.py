e0c4772b0c4c5b8a3f963ecab7b1b5759e83391a
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2021-11-21 13:05:08 +0530

    release 0.1.0 getTransactionInfo(), updated readme and other fixes

--- test.py
+++
@@ -1,5 +0,0 @@
-import deso
-import json
-with open("test.json", "w") as file:
-    publicKey = "BC1YLhBLE1834FBJbQ9JU23JbPanNYMkUsdpJZrFVqNGsCe7YadYiUg"
-    json.dump(deso.Users.getTransactionInfo(publicKey=publicKey), file)
