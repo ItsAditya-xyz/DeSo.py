b29428548f47b01def94c312a22b902519653e83
Sunrit Jana <warriordefenderz@gmail.com>
2021-11-27 18:42:55 +0000

    Refactor basic code.

--- deso/__init__.py
+++ deso/__init__.py
@@ -6,3 +6,10 @@ from deso.Trade import Trade
 from deso.Post import Post
 from deso.Message import Message
 from deso.Nft import Nft
+
+# Library conf
+__author__ = "Aditya Chaudhary"
+__email__ = "chaudharyaditya0005@gmail.com"
+__version__ = "0.1.3"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021 Aditya Chaudhary"
