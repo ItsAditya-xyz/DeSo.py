28362675984662f50de0b730549c8d7367cdd319
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2021-12-05 22:07:34 +0530

    added like transaction

--- deso/Post.py
+++ deso/Post.py
@@ -1,10 +1,11 @@
 import requests
-from deso.Route import getRoute
+from deso.Route import ROUTE, getRoute
 from deso.Sign import Sign_Transaction
 import binascii
 from ecdsa import SigningKey, SECP256k1
 from ecdsa.ecdsa import Public_key
 import jwt
+
 
 class Post:
     def __init__(self, seedHex, publicKey):
@@ -22,9 +23,9 @@ class Post:
         endpointURL = ROUTE + "upload-image"
         payload = {'UserPublicKeyBase58Check': self.PUBLIC_KEY,
                    'JWT': encoded_jwt}
-        response = requests.request("POST", endpointURL, data=payload, files=fileList)
+        response = requests.request(
+            "POST", endpointURL, data=payload, files=fileList)
         return response.json()
-
 
     def send(self, content, imageUrl=[], postExtraData={}):
         header = {
@@ -32,16 +33,16 @@ class Post:
         }
 
         payload = {"UpdaterPublicKeyBase58Check": self.PUBLIC_KEY,
-                "PostHashHexToModify": "",
-                "ParentStakeID": "",
-                "Title": "",
-                "BodyObj": {"Body": content, "ImageURLs": imageUrl},
-                "RecloutedPostHashHex": "",
-                "PostExtraData": postExtraData,
-                "Sub": "",
-                "IsHidden":  False,
-                "MinFeeRateNanosPerKB": 1000
-                }
+                   "PostHashHexToModify": "",
+                   "ParentStakeID": "",
+                   "Title": "",
+                   "BodyObj": {"Body": content, "ImageURLs": imageUrl},
+                   "RecloutedPostHashHex": "",
+                   "PostExtraData": postExtraData,
+                   "Sub": "",
+                   "IsHidden":  False,
+                   "MinFeeRateNanosPerKB": 1000
+                   }
         ROUTE = getRoute()
         endpointURL = ROUTE + "submit-post"
         res = requests.post(endpointURL, json=payload, headers=header)
@@ -54,26 +55,47 @@ class Post:
         submitPayload = {"TransactionHex": signedTransactionHex}
         endpointURL = ROUTE + "submit-transaction"
         submitResponse = requests.post(endpointURL, json=submitPayload)
-        return {"status":submitResponse.status_code , "postHashHex": submitResponse.json()["TxnHashHex"]} # returns 200
+        # returns 200
+        return {"status": submitResponse.status_code, "postHashHex": submitResponse.json()["TxnHashHex"]}
 
     def mint(self, postHashHex, minBidDeSo,  copy=1, creatorRoyality=5, coinHolderRoyality=10, isForSale=True):
-            ROUTE = getRoute()
-            endpointURL = ROUTE + "create-nft"
-            payload = {"UpdaterPublicKeyBase58Check": self.PUBLIC_KEY,
-                        "NFTPostHashHex": postHashHex,
-                        "NumCopies": copy,
-                        "NFTRoyaltyToCreatorBasisPoints": round(creatorRoyality*100),
-                        "NFTRoyaltyToCoinBasisPoints": round(coinHolderRoyality*100), 
-                        "HasUnlockable": False, 
-                        "IsForSale": isForSale, "MinBidAmountNanos": round( minBidDeSo * 1e9),
-                        "MinFeeRateNanosPerKB": 1000}
-            response = requests.post(endpointURL, json=payload)
-            transactionHex = response.json()["TransactionHex"]
-            signedTransactionHex = Sign_Transaction(
-                self.SEED_HEX, transactionHex
-            )  # txn signature
+        ROUTE = getRoute()
+        endpointURL = ROUTE + "create-nft"
+        payload = {"UpdaterPublicKeyBase58Check": self.PUBLIC_KEY,
+                   "NFTPostHashHex": postHashHex,
+                   "NumCopies": copy,
+                   "NFTRoyaltyToCreatorBasisPoints": round(creatorRoyality*100),
+                   "NFTRoyaltyToCoinBasisPoints": round(coinHolderRoyality*100),
+                   "HasUnlockable": False,
+                   "IsForSale": isForSale, "MinBidAmountNanos": round(minBidDeSo * 1e9),
+                   "MinFeeRateNanosPerKB": 1000}
+        response = requests.post(endpointURL, json=payload)
+        transactionHex = response.json()["TransactionHex"]
+        signedTransactionHex = Sign_Transaction(
+            self.SEED_HEX, transactionHex
+        )  # txn signature
 
-            submitPayload = {"TransactionHex": signedTransactionHex}
-            endpointURL = ROUTE + "submit-transaction"
-            submitResponse = requests.post(endpointURL, json=submitPayload)
-            return submitResponse.status_code  # returns 200 if mint is succesful
+        submitPayload = {"TransactionHex": signedTransactionHex}
+        endpointURL = ROUTE + "submit-transaction"
+        submitResponse = requests.post(endpointURL, json=submitPayload)
+        return submitResponse.status_code  # returns 200 if mint is succesful
+
+    # write transactions on posts goes here
+    def like(self, postHashHex, isLike=True):
+        ROUTE = getRoute()
+        endpointURL = ROUTE + "create-like-stateless"
+        payload = {"ReaderPublicKeyBase58Check": self.PUBLIC_KEY,
+                   "LikedPostHashHex": postHashHex,
+                   "IsUnlike": not isLike,
+                   "MinFeeRateNanosPerKB": 1000}
+
+        response = requests.post(endpointURL, json=payload)
+        transactionHex = response.json()["TransactionHex"]
+        signedTransactionHex = Sign_Transaction(
+            self.SEED_HEX, transactionHex
+        )  # txn signature
+
+        submitPayload = {"TransactionHex": signedTransactionHex}
+        endpointURL = ROUTE + "submit-transaction"
+        submitResponse = requests.post(endpointURL, json=submitPayload)
+        return submitResponse.status_code  # returns 200 if like is succesful
