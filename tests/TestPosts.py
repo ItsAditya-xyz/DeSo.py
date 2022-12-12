c0b6b12703b8da4233beab1f81a467d8298a3573
Mo Elzubeir <elzubeir@gmail.com>
2022-12-12 17:38:19 -0600

    almost pep8 compliant :)

--- tests/TestPosts.py
+++ tests/TestPosts.py
@@ -21,19 +21,24 @@ class TestPosts(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestPosts, self).__init__(*args, **kwargs)
-        self.post_hash = '75b0244b1abc19e3e7ae0cf36f43ecb12588aa30ae48db7992edf3fb94d289ad'
-        self.nft_post = '2298e051237a8b831aa27d4748d759a8002dd1ab448195ae89d888446ee444e3'
-        self.publicReaderKey = 'BC1YLiy1Ny1btpBkaNHBaUD5D9xX8PhdgeToPn3Fq95RhCMYQVW1Anw'
+        self.post_hash = '75b0244b1abc19e3e7ae0cf36f43ecb12588aa30ae4'\
+            '8db7992edf3fb94d289ad'
+        self.nft_post = '2298e051237a8b831aa27d4748d759a8002dd1ab4481'\
+            '95ae89d888446ee444e3'
+        self.publicReaderKey = 'BC1YLiy1Ny1btpBkaNHBaUD5D9xX8PhdgeToP'\
+            'n3Fq95RhCMYQVW1Anw'
 
         self.posts = Posts(readerPublicKey=self.publicReaderKey)
-
 
     def test_get_single_post(self):
         """Test the getSinglePost method."""
         try:
             response = self.posts.getSinglePost(self.post_hash)
+        except Exception:
+            raise
         finally:
-            sys.stdout.write(f'\ngetSinglePost() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetSinglePost() using node: {self.posts.NODE_URL}\n')
         self.assertEqual(response.status_code, 200)
 
     def test_get_posts_stateless(self):
@@ -43,10 +48,11 @@ class TestPosts(unittest.TestCase):
                 postHashHex=self.post_hash,
                 numToFetch=10,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetPostsStateless() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetPostsStateless() using node: {self.posts.NODE_URL}\n')
         self.assertEqual(response.status_code, 200)
 
     def test_get_posts_for_public_key(self):
@@ -55,10 +61,12 @@ class TestPosts(unittest.TestCase):
             response = self.posts.getPostsForPublicKey(
                 publicKey=self.publicReaderKey,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetPostsForPublicKey() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetPostsForPublicKey() using node: '
+                f'{self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
 
@@ -68,13 +76,14 @@ class TestPosts(unittest.TestCase):
             response = self.posts.getDiamondsForPost(
                 postHashHex=self.post_hash,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetDiamondsForPost() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetDiamondsForPost() using node: '
+                f'{self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
-
 
     def test_get_likes_for_post(self):
         """Test the getLikesForPost method."""
@@ -82,13 +91,13 @@ class TestPosts(unittest.TestCase):
             response = self.posts.getLikesForPost(
                 postHashHex=self.post_hash,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetLikesForPost() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetLikesForPost() using node: {self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
-
 
     def test_get_quotereposts_for_post(self):
         """Test the getQuoteRepostsForPost method."""
@@ -96,10 +105,12 @@ class TestPosts(unittest.TestCase):
             response = self.posts.getQuoteRepostsForPost(
                 postHashHex=self.post_hash,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetQuoteRepostsForPost() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetQuoteRepostsForPost() using node: '
+                f'{self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
 
@@ -109,10 +120,12 @@ class TestPosts(unittest.TestCase):
             response = self.posts.getNFTEntriesForNFTPost(
                 postHashHex=self.nft_post,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetNFTEntriesForNFTPost() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetNFTEntriesForNFTPost() using node: '
+                f'{self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
 
@@ -122,10 +135,12 @@ class TestPosts(unittest.TestCase):
             response = self.posts.getNFTBidsForNFTPost(
                 postHashHex=self.nft_post,
             )
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetNFTBidsForNFTPostPost() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetNFTBidsForNFTPostPost() using node: '
+                f'{self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
 
@@ -133,13 +148,13 @@ class TestPosts(unittest.TestCase):
         """Test the getHotFeed method."""
         try:
             response = self.posts.getHotFeed()
-        except:
+        except Exception:
             raise
         finally:
-            sys.stdout.write(f'\ngetHotFeed() using node: {self.posts.NODE_URL}\n')
+            sys.stdout.write(
+                f'\ngetHotFeed() using node: {self.posts.NODE_URL}\n')
 
         self.assertEqual(response.status_code, 200)
-
 
 
 if __name__ == "__main__":
---
+++ tests/TestUser.py
@@ -0,0 +1,226 @@
+"""
+Unit tests for the deso.posts module.
+"""
+import sys
+import importlib
+import unittest
+# making sure we're not importing some other version of deso
+# there is certainly a better way to do this, so if you know it
+# pls feel free to contribute it :)
+MODULE_PATH = "../deso/__init__.py"
+MODULE_NAME = "deso"
+spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
+module = importlib.util.module_from_spec(spec)
+sys.modules[spec.name] = module
+spec.loader.exec_module(module)
+from deso import User
+
+
+class TestUser(unittest.TestCase):
+    """Test the Derived class."""
+
+    def __init__(self, *args, **kwargs):
+        super(TestUser, self).__init__(*args, **kwargs)
+        self.post_hash = '75b0244b1abc19e3e7ae0cf36f43ecb12588aa30'\
+            'ae48db7992edf3fb94d289ad'
+        self.nft_post = '2298e051237a8b831aa27d4748d759a8002dd1ab4'\
+            '48195ae89d888446ee444e3'
+        self.publicReaderKey = 'BC1YLiy1Ny1btpBkaNHBaUD5D9xX8Phdge'\
+            'ToPn3Fq95RhCMYQVW1Anw'
+        self.daoPublicKey = 'BC1YLj3zNA7hRAqBVkvsTeqw7oi4H6ogKiAFL'\
+            '1VXhZy6pYeZcZ6TDRY'
+        self.pkList = [
+            'BC1YLhBLE1834FBJbQ9JU23JbPanNYMkUsdpJZrFVqNGsCe7YadYiUg',
+            self.publicReaderKey,
+        ]
+        self.username = 'deso'
+        self.user = User()
+
+    def test_get_user_profile_pubkey(self):
+        """Test the getSingleProfile method using public key"""
+        try:
+            response = self.user.getSingleProfile(
+                publicKey=self.publicReaderKey)
+        finally:
+            sys.stdout.write(
+                f'\ngetSingleProfile() using node: {self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_profile_username(self):
+        """Test the getSingleProfile method using username"""
+        try:
+            response = self.user.getSingleProfile(username=self.username)
+        finally:
+            sys.stdout.write(
+                f'\ngetSingleProfile() using node: {self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_users_stateless(self):
+        """Test the getUsersStateless method"""
+        try:
+            response = self.user.getUsersStateless(
+                listOfPublicKeys=self.pkList
+            )
+        finally:
+            sys.stdout.write(
+                f'\ngetUsersStateless() using node: {self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_messages_stateless(self):
+        """Test the getMessagesStateless method"""
+        try:
+            response = self.user.getMessagesStateless(
+                publicKey=self.publicReaderKey,
+                numToFetch=10,
+            )
+        finally:
+            sys.stdout.write(
+                f'\ngetMessagesStateless() using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_notifications(self):
+        """Test the getNotifications method"""
+        try:
+            response = self.user.getNotifications(
+                publicKey=self.publicReaderKey,
+                numToFetch=10,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetNotificaitons() using node: {self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_nfts(self):
+        """Test the getNFTs method"""
+        try:
+            response = self.user.getNFTs(
+                userPublicKey=self.publicReaderKey,
+                isForSale=True
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetNFTs() using node: {self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_derived_keys(self):
+        """Test the getDerivedKeys method"""
+        try:
+            response = self.user.getDerivedKeys(
+                publicKey=self.publicReaderKey,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetDerivedKeys() using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_transaction_info(self):
+        """Test the getTransactionInfo method"""
+        try:
+            response = self.user.getTransactionInfo(
+                publicKey=self.publicReaderKey,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetTransactionInfo() using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_holders_for_public_key(self):
+        """Test the getHoldersForPublicKey method"""
+        try:
+            response = self.user.getHoldersForPublicKey(
+                self.publicReaderKey,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetHoldersForPublicKey() using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_dao_coin_limit_orders(self):
+        """Test the getDaoCoinLimitOrders method"""
+        try:
+            response = self.user.getDaoCoinLimitOrders(
+                self.publicReaderKey,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetDaoCoinLimitOrders() using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_dao_coin_price(self):
+        """Test the getDaoCoinPrice method"""
+        try:
+            response = self.user.getDaoCoinPrice(
+                self.daoPublicKey
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetDaoCoinPrice() using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertIsInstance(response, float)
+
+    def test_get_follows_stateless(self):
+        """Test the getFollowsStateless method"""
+        try:
+            response = self.user.getFollowsStateless(
+                username=self.username,
+                getFollowing=False,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetFollowsStateless(followers) using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+        try:
+            response = self.user.getFollowsStateless(
+                username=self.username,
+                getFollowing=True,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetFollowsStateless(following) using node: '
+                f'{self.user.NODE_URL}\n')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_diamonds_for_public_key(self):
+        """Test the getDiamondsForPublicKey method"""
+        try:
+            response = self.user.getDiamondsForPublicKey(
+                publicKey=self.publicReaderKey,
+                received=True,
+            )
+        except Exception:
+            raise
+        finally:
+            sys.stdout.write(
+                f'\ngetDiamondsForPublicKey(received) using node: '
+                f'{self.user.NODE_URL}\n')
+
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
